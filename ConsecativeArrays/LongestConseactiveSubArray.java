eeb3a82e4b195c7c823917201ef25a4087ab9a50
Rakesh <dr8452799@gmail.com>
2025-10-26 15:18:43 +0530

    Implement Longest Consecutive Subarray finder

---
+++ ConsecativeArrays/LongestConseactiveSubArray.java
@@ -0,0 +1,41 @@
+package ConsecativeArray;
+import java.util.*;
+public class LongestConsecativeSubarray {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int ar [] = new int [n];
+		for(int i=0;i<ar.length;i++)
+		{
+			ar[i]=scan.nextInt();
+		}
+		LongestConsecutiveSubarray(ar);
+	}
+	static void LongestConsecutiveSubarray(int ar [])
+	{
+		int count=1,max=0;
+		for(int i=0;i<ar.length-1;i++)
+		{
+			if(ar[i+1]-ar[i]==1)
+			{
+				count++;
+			}
+			else {
+				if(count>max)
+				{
+					max=count;	
+				}
+				count=1;
+			}
+		}
+		// For the Last Value
+		if(count>max)
+		{
+			max=count;
+		}
+		System.out.println(max);
+	}
+	
+
+}
